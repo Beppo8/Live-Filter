ae96f8a2f2b9e1e5395c08ca5cf130a08e32b7e3
beppo <beppo@localhost.localdomain>
2021-02-01 08:47:14 -0600

    Populating seeds

--- lib/live_view_studio/boats.ex
+++ lib/live_view_studio/boats.ex
@@ -21,7 +21,24 @@ defmodule LiveViewStudio.Boats do
     Repo.all(Boat)
   end
 
+  def list_boats(criteria) when is_list(criteria) do
+    query = from(b in Boat)
 
+    Enum.reduce(criteria, query, fn
+      {:type, ""}, query ->
+        query
+
+      {:type, type}, query ->
+        from q in query, where: q.type == ^type
+
+      {:prices, [""]}, query ->
+        query
+
+      {:prices, prices}, query ->
+        from q in query, where: q.price in ^prices
+    end)
+    |> Repo.all()
+  end
 
   @doc """
   Gets a single boat.
